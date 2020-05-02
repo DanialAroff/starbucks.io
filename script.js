8c21c8e893575cd1821b8a4310f7e226f57a924e
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2020-05-02 20:39:47 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.querySelector("#submit-button").addEventListener('click', function() {
-    window.open('/blank.html', '_blank');
+// document.querySelector("#submit-button").addEventListener('click', function() {
+//     window.open('/blank.html', '_blank');
+// });
+
+document.querySelector("#button-1").addEventListener('click', function() {
+    window.open('https://card.sbux.com.my/', '_blank');
+});
+document.querySelector("#button-2").addEventListener('click', function() {
+    window.open('https://card.sbux.com.my/index2.php?module=member&page=signin', '_blank');
 });
